71d7e9b1365694800ed99cd859374994c54ceffb
vlajksanyee <vlajksanyi@gmail.com>
2025-11-02 10:32:43 +0100

    feat: save screenshots instead of html files

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from playwright.async_api import async_playwright
 from waybackpy import WaybackMachineCDXServerAPI
+import asyncio
 import requests
 import os
 
@@ -16,37 +18,48 @@ def setup() -> Config:
         url = str(input('URL: ')),
         start_year = int(input('Start year: ')),
         end_year = int(input('End year: '))
-    )   
+    )
 
-def get_snapshots(cfg: Config):
+async def make_screenshot_playwright(url: str, filename: str):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch() 
+        page = await browser.new_page()
+        try:
+            await page.goto(url, timeout=120000)
+            await page.wait_for_selector('body', state='visible', timeout=30000)
+            await page.screenshot(path=os.path.join("snapshots", filename), full_page=True)
+            print(f"Screenshot saved: {filename}")
+        except Exception as e:
+            print(f"Error saving screenshot: {filename}: {e}")
+        finally:
+            await browser.close()
+
+async def get_snapshots(cfg: Config):
     os.makedirs(cfg.save_folder, exist_ok=True)
+    screenshot_tasks = []
     for year in range(cfg.start_year, cfg.end_year + 1):
         print(f'=== {year} ===')
         cdx = WaybackMachineCDXServerAPI(url=cfg.url, user_agent=cfg.user_agent)
         
         try:
-            snapshot = cdx.near(year=int(f'{year}0101'))
-            snapshot_url = snapshot.archive_url
+            snapshot = cdx.near(year=int(f'{year}0201'))
+            snapshot_url = snapshot.archive_url[:-3] if snapshot.archive_url.endswith("id_") else snapshot.archive_url
+            snapshot_id = snapshot.timestamp
+            filename = f'{year}_{snapshot_id}.png'
             print(f'Snapshot: {snapshot_url}')
 
-            response = requests.get(snapshot_url)
-            snapshot_id = snapshot.timestamp
-            
-            # Insert base tag to make the browser load images from Wayback Machine
-            html_content = response.text.replace(
-                "<head>",
-                f'<head><base href="https://web.archive.org/web/{snapshot_id}/">'
-            )
-
-            filename = os.path.join(cfg.save_folder, f'{year}.html')
-            with open(filename, 'w', encoding='utf-8') as f:
-                f.write(html_content)
-
-            print(f'Saved: {filename}\n')
+            task = make_screenshot_playwright(url=snapshot_url, filename=filename)
+            screenshot_tasks.append(task)
 
         except Exception as e:
             print(f'Error at year {year}: {e}\n')
 
+    if screenshot_tasks:
+        print("\nCreating screenshots\n")
+        await asyncio.gather(*screenshot_tasks)
+    else:
+        print("No screenshots to create")
+
 if __name__ == '__main__':
     config = setup()
-    get_snapshots(cfg=config)
+    asyncio.run(get_snapshots(cfg=config))
